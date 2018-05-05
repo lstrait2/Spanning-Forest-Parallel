6da5017822de0640a65e419f7170f3fe2c099c98
Lance Strait <lancestrait10@hotmail.com>
2018-05-05 12:20:45 -0500

    Removed extra spaces Graph file

--- Graph.java
+++ Graph.java
@@ -8,17 +8,13 @@ public class Graph
 	{
 		final int index;
 		Node ancestor;
-	
 		Node(int index)
 		{
 			this.index = index;
 			ancestor = this;
 		}
-		
-
 	}
-
-
+	
 	static class Edge
 	{
 		final Node u, v;
@@ -32,8 +28,6 @@ public class Graph
 
 	}
 
-	
-
 	ArrayList<Edge> edges;
 	Node[] nodes;
 
@@ -46,35 +40,26 @@ public class Graph
 	static Graph readEdgeGraph(String file) throws IOException
 	{
 		BufferedReader reader = new BufferedReader(new FileReader(file));
-		
 		Graph g = new Graph();
-		
 		if(!"EdgeArray".equals(reader.readLine()))
 			throw new IOException("invalid edge graph format");
-
 		while(true)
 		{
 			String line;
-
 			try
 			{
 				line = reader.readLine();
-			}catch(EOFException e){ break;}
-			
-
+			}
+			catch(EOFException e){ break;}
 			if(line == null)
 				break;
-			
 			String[] words = line.split("[ \t]+");
 			if(words.length != 2)
 				throw new IOException("invalid edge graph format");
-			
 			int u = Integer.parseInt(words[0]);
 			int v = Integer.parseInt(words[1]);
-
 			Node U = g.getVertex(u);
 			Node V = g.getVertex(v);
-		
 			g.addEdge(U,V);
 		}
 		return g;
@@ -84,10 +69,8 @@ public class Graph
 	{
 		if(nodes.length < n)
 			nodes = Arrays.copyOf(nodes, n+1+ n/2);
-		
 		if(nodes[n] == null)
 			nodes[n] = new Node(n);
-		
 		return nodes[n];
 	}
 
@@ -96,6 +79,4 @@ public class Graph
 		Edge edge = new Edge(u,v);
 		edges.add(edge);
 	}
-
-
 }
